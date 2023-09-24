7ded2f081dbc34ccd23b6d863d3c976003363aab
Enron Musk <lukerivard17@gmail.com>
2023-09-23 20:42:50 -0400

    moved utilities from functions into utils.py

--- ReviewGPT/functions.py
+++ ReviewGPT/functions.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import math as m
 import matplotlib as plt
 import xgboost as xgb
 from xgboost import XGBClassifier
@@ -357,14 +356,6 @@ drop_cols=["reviewerID", "asin", "reviewerName", "helpful", "reviewText", "summa
 def downloadData(path : str, n : int) -> pd.DataFrame:
     return pd.read_json(path, lines=True).head(n)
 
-"""
-Checks if a word is present in review or review title. Outputs 1 for present 0 for not.
-
-@params
-word : the word to be checked.
-row : the row to be searched for the word.
-"""
-
 def wordCheck(row : pd.DataFrame, word : str) -> bool:
     return 1 if (word in row['reviewText'].lower() or word in row['summary'].lower()) else 0
 
@@ -384,46 +375,3 @@ def transformData(data : pd.DataFrame) -> pd.DataFrame:
         data.drop(col, axis=1, inplace=True)
 
     return data
-
-def frequencyHistogram(colname : str, data : pd.DataFrame):
-
-    counts = data[colname].value_counts()
-    proportion = counts/counts.sum()
-
-    #Create histogram plot here
-    plot = proportion.plot(kind = 'bar', alpha = 0.6, color='green', edgecolor = 'black')
-    plot.grid(alpha = 0.3)
-    plot.set_xlabel('Overall Score', fontweight='bold')
-    plot.set_ylabel('Frequency', fontweight='bold')
-    plot.set_title('Overall Score Distribution')
-    
-    
-def returnKeywords() -> None:
-    return key_words
-
-#Prints simple diagnostics
-def accuracy(Y_test : pd.DataFrame, predictions : list) -> None:
-    accuracy = accuracy_score(Y_test, predictions)
-    report = classification_report(Y_test, predictions)
-
-    print(f'Accuracy: {accuracy}')
-    print(f'Classification Report:\n{report}')
-
-#Creates and prints a simple confusion matrix
-def confusionMatrix(Y_test : pd.DataFrame, predictions : list) -> None:
-    tn, fp, fn, tp = confusion_matrix(Y_test, predictions).ravel()
-
-    #Print the matrix
-    print("Confusion Matrix:")
-    print("")
-    print(f"False Positives: {fp}")
-    print(f"False Negatives: {fn}")
-    print(f"True Positives: {tp}")
-    print(f"True Negatives: {tn}")
-
-#Prints importance scores of features
-def importance(model : XGBClassifier) -> None:
-    plot = xgb.plot_importance(model, max_num_features=15, importance_type='gain', values_format = '{v:.2f}')
-    plot.grid(alpha=0.2)
-    plot.set_xlabel('Gain', weight = 'bold')
-    plot.set_ylabel('Features', weight = 'bold')
